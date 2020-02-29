6576a8d423c394138f0cae4fe9d5f152b535fb62
Auganbay Rymkhan <37842023+Augan93@users.noreply.github.com>
2020-02-29 21:15:26 +0600

    custom hooks

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -8,6 +8,8 @@ const IngredientForm = React.memo(props => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
 
+    console.log('RENDERING INGREDIENT_FORM');
+
     const submitHandler = event => {
         event.preventDefault();
         props.onAddIngredient({
